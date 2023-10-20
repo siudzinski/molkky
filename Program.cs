4d261e48507c05dc9226d408d126105cb8c592e6
siudzinski <tomek.siudzinski@gmail.com>
2023-10-20 19:44:57 +0200

    baseaddress workaround fix 2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var baseAddress = builder.HostEnvironment.IsDevelopment() 
     ? new Uri(builder.HostEnvironment.BaseAddress)
-    : new Uri("https://siudzinski.github.io/");
+    : new Uri("https://siudzinski.github.io/molkky/");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseAddress });
 builder.Services.AddTransient<GameSessionStorage>();
