90b72114108394700ff0d8739c3d1f3bc9e0afd5
siudzinski <tomek.siudzinski@gmail.com>
2023-10-21 19:38:36 +0200

    change layout to BlazorBoostrap

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddTransient<GameSessionStorage>();
 
 await builder.Build().RunAsync();
